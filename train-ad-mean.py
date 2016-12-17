6be2e7177008c4d7310a4ca17343b0063e419bf0
Alexey Noskov <alexey.noskov@gmail.com>
2016-12-17 16:52:36 +0300

    Compute present and future scores besides total

--- train-ad-mean.py
+++ train-ad-mean.py
@@ -17,7 +17,7 @@ def fit_predict(train_file, pred_file):
     del train, train_pos
 
     test = pd.read_csv(pred_file, dtype=np.int32)
-    test['score'] = test['ad_id'].map(ad_pos_cnt).fillna(0) / (test['ad_id'].map(ad_cnt).fillna(0) + reg)
+    test['pred'] = test['ad_id'].map(ad_pos_cnt).fillna(0) / (test['ad_id'].map(ad_cnt).fillna(0) + reg)
 
     return test
 
@@ -29,18 +29,21 @@ pred = fit_predict(val_split[0], val_split[1])
 
 print "Scoring..."
 
-score = score_prediction(pred)
-name = gen_prediction_name('ad-mean', score)
+present_score, future_score, total_score = score_prediction(pred)
+name = gen_prediction_name('ad-mean', total_score)
 
-print "  Score: %.5f" % score
+print "  Present score: %.5f" % present_score
+print "  Future score: %.5f" % future_score
+print "  Total score: %.5f" % total_score
 
-pred[['display_id', 'ad_id', 'score']].to_pickle('preds/%s.pickle' % name)
+pred[['display_id', 'ad_id', 'pred']].to_pickle('preds/%s-val.pickle' % name)
 
 ## Predicting
 
 print "Running on full split..."
 
 pred = fit_predict(full_split[0], full_split[1])
+pred[['display_id', 'ad_id', 'pred']].to_pickle('preds/%s-test.pickle' % name)
 
 print "  Generating submission..."
 subm = gen_submission(pred)
--- util/__init__.py
+++ util/__init__.py
@@ -1,6 +1,9 @@
 import pandas as pd
+import numpy as np
 
 import datetime
+
+from .meta import val_split_time
 
 
 def gen_prediction_name(model_name, score):
@@ -8,7 +11,7 @@ def gen_prediction_name(model_name, score):
 
 
 def gen_submission(pred):
-    pred = pred.sort_values(['display_id', 'score'], ascending=[True, False])[['display_id', 'ad_id']]
+    pred = pred.sort_values(['display_id', 'pred'], ascending=[True, False])[['display_id', 'ad_id']]
 
     res_idx = []
     res_ads = []
@@ -25,23 +28,38 @@ def gen_submission(pred):
 
 
 def score_prediction(pred):
-    pred = pred.sort_values(['display_id', 'score'], ascending=[True, False])[['display_id', 'clicked']]
+    pred = pred.sort_values(['display_id', 'pred'], ascending=[True, False])[['display_id', 'clicked']]
+    pred = pd.merge(pred, pd.read_csv("../input/events.csv.zip", dtype=np.int32, index_col=0, usecols=[0, 3]), left_on='display_id', right_index=True)
 
     cur_idx = None
     cur_rank = None
 
-    score_sum = 0.0
-    score_cnt = 0
+    future_score_sum = 0.0
+    future_score_cnt = 0
+
+    present_score_sum = 0.0
+    present_score_cnt = 0
 
     for t in pred.itertuples():
         if cur_idx == t.display_id:
             cur_rank += 1
         else:
-            score_cnt += 1
+            if t.timestamp >= val_split_time:
+                future_score_cnt += 1
+            else:
+                present_score_cnt += 1
+
             cur_idx = t.display_id
             cur_rank = 1
 
         if t.clicked == 1:
-            score_sum += 1.0 / cur_rank
+            if t.timestamp >= val_split_time:
+                future_score_sum += 1.0 / cur_rank
+            else:
+                present_score_sum += 1.0 / cur_rank
 
-    return score_sum / score_cnt
+    present_score = present_score_sum / present_score_cnt
+    future_score = future_score_sum / future_score_cnt
+    total_score = (present_score_sum + future_score_sum) / (present_score_cnt + future_score_cnt)
+
+    return present_score, future_score, total_score
