b0984a8ad5597f63711389bd7dd2297c555ff470
Alexey Noskov <alexey.noskov@gmail.com>
2016-12-17 12:46:31 +0300

    Initial code structure and data split script

---
+++ prepare-split.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+
+from util.meta import val_split
+
+print "Loading train..."
+
+train = pd.read_csv("../input/clicks_train.csv.zip", dtype=np.int32, index_col=0)
+
+print "Loading events..."
+
+events = pd.read_csv("../input/events.csv.zip", dtype=np.int32, index_col=0, usecols=[0, 3])  # Load events
+events = events.loc[events.index.intersection(train.index.unique())]  # Take only train events
+
+print "Splitting events..."
+
+# Select display_ids for val - consists of time-based and sampled parts
+train_is_val = train.index.isin(events[(events['timestamp'] >= 950400000) | (events.index % 6 == 5)].index)
+
+del events
+
+print "Saving..."
+
+train[~train_is_val].to_csv(val_split[0], index=True, compression='bz2')
+train[train_is_val].to_csv(val_split[1], index=True, compression='bz2')
+
+print "Done."
---
+++ util/meta.py
@@ -0,0 +1,8 @@
+import os
+
+input_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..', 'input')
+cache_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'cache')
+
+
+full_split = (os.path.join(input_dir, 'clicks_train.csv.zip'), os.path.join(input_dir, 'clicks_test.csv.zip'))
+val_split = (os.path.join(cache_dir, 'clicks_val_train.csv.bz2'), os.path.join(cache_dir, 'clicks_val_test.csv.bz2'))
