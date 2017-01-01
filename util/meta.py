73c5c571655e5fc67434002b4e9c3b67a8c2e1de
Alexey Noskov <alexey.noskov@gmail.com>
2017-01-01 17:34:43 +0300

    Test split time

--- util/meta.py
+++ util/meta.py
@@ -8,3 +8,4 @@ full_split = (os.path.join(input_dir, 'clicks_train.csv.gz'), os.path.join(input
 val_split = (os.path.join(cache_dir, 'clicks_val_train.csv.gz'), os.path.join(cache_dir, 'clicks_val_test.csv.gz'))
 
 val_split_time = 950400000
+test_split_time = 1123200000
