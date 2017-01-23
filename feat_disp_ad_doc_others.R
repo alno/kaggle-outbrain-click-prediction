7dd1f1f67016bcdad9f5aa8f3141da66cf2e6c11
Alexey Noskov <alexey.noskov@gmail.com>
2017-01-23 23:56:16 +0300

    Update doc-ad-others generation, no conversion required

--- feat_disp_ad_doc_others.R
+++ feat_disp_ad_doc_others.R
@@ -1,6 +1,6 @@
 # """
 # Here we create a feature keyed on display_id, where for each display, we hash the interaction
-# of the display document, and each of the ads on the display. 
+# of the display document, and each of the ads on the display.
 # We use this feature by joining it back to the clicks_train and clicks_test files, so that
 # each row has information on the ads it it is competing against.
 # To run this script, execute the following command from the main folder :
@@ -20,17 +20,19 @@ gc()
 
 D = 2^22  # Hash value
 
+input_dir = Sys.getenv("INPUT", "../input")
+
 cat("load and join the clicks and events data")
-ctrnraw = fread("input/clicks_train.csv", select=c("display_id", "ad_id"))
-ctstraw = fread("input/clicks_test.csv", select=c("display_id", "ad_id"))
+ctrnraw = fread(paste0("gunzip -c ", input_dir, "/clicks_train.csv.gz"), select=c("display_id", "ad_id"))
+ctstraw = fread(paste0("gunzip -c ", input_dir, "/clicks_test.csv.gz"), select=c("display_id", "ad_id"))
 craw = rbind(ctrnraw, ctstraw)
 rm(ctrnraw, ctstraw); gc()
-event = fread("input/events.csv", select=c("display_id", "document_id"))
+event = fread(paste0("gunzip -c ", input_dir, "/events.csv.gz"), select=c("display_id", "document_id"))
 craw = merge(craw, event, all.x = T, by = "display_id")
 rm(event); gc()
 
 cat("Get the interaction of the ad and the dipslay documents")
-# We add a '99' to avoid colisions; eg. if we just paste them together, 
+# We add a '99' to avoid colisions; eg. if we just paste them together,
 # the diplay_id '1' and ad_id '1000' would get the same value as display_id '1100' and ad_id '1'
 craw[,document_ad_id:=paste0(document_id, "99", ad_id, sep="")]
 craw[,`:=`(document_id=NULL, ad_id=NULL)]
@@ -47,11 +49,11 @@ craw[,document_ad_id:=as.numeric(document_ad_id)%%D]
 
 cat("Now we aggregate each interaction per display")
 setkeyv(craw, "display_id")
-craw = craw[,(paste0(paste("5:",document_ad_id,":1", sep=""), collapse = " ")), by=display_id]
+craw = craw[,(paste0(document_ad_id, collapse = " ")), by=display_id]
 setnames(craw, c("display_id", "document_ad_id"))
 setkeyv(craw, "display_id")
 
 cat("Write out the file")
-write.csv(craw, gzfile("document_ad_id_others.csv.gz"), row.names = F)
+write.csv(craw, gzfile("cache/doc_ad_others.csv.gz"), row.names = F, quote = F)
 gc()
 
